a39663345118bee813820ee7abb6d08a7ccd13a2
isa <isabel@juntostudio.com>
2016-11-01 10:14:04 -0300

    Add new speakers

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ jQuery(function($) {
     updateURL: false
   });
 });
+
+// Speaker Modal
+
+$('#speakerModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var speakerName = button.find('h3').text() // Extract info from data-* attributes
+  var speakerBio = button.data('bio')
+  var speakerPhoto = button.find('img').attr("src")
+
+  var modal = $(this)
+  modal.find('#speakerName').text(speakerName)
+  modal.find('#speakerBio').text(speakerBio)
+  modal.find('#speakerPhoto').attr("src",speakerPhoto)
+})
